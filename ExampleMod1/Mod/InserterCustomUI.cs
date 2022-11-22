e112986f0aec7c0d3b797ebef38bb4c8af26a11b
Lennon <hitzemanl@yahoo.com>
2022-11-21 23:19:37 -0800

    Remade the UI; ui now has inventory and you can click on items to pick them up. completely rebuild how the ui looks on the top half and made it so scaling work

--- ExampleMod1/Mod/InserterCustomUI.cs
+++ ExampleMod1/Mod/InserterCustomUI.cs
@@ -13,40 +13,41 @@ using StardewValley.Minigames;
 using System.Reflection;
 using StardewModdingAPI;
 using StardewValley.Objects;
+using StardewValley.Tools;
 
 namespace ExampleMod1
 {
-    public class InserterCustomUI : IClickableMenu
+    public class InserterCustomUI : MenuWithInventory
     {
         private RootElement ui;
         private Table table;
         private InserterObject inserterInstance;
-        public InventoryMenu ItemsToGrabMenu;
         IList<Item> WhiteListItems;
+        private int heightOffset = -120;
 
-        public InserterCustomUI() : base((Game1.uiViewport.Width - 900) / 2, (Game1.uiViewport.Height - (Game1.uiViewport.Height - 100)) / 2, 900, (Game1.uiViewport.Height - 100))
+        public InserterCustomUI() : base(null, okButton: false, trashCan: false, 0,0)//12, 132)//: base((Game1.uiViewport.Width - 900) / 2, (Game1.uiViewport.Height - (Game1.uiViewport.Height - 100)) / 2, 900, (Game1.uiViewport.Height - 100))
         {
-            ModEntry._Monitor.Log($"UI Set", LogLevel.Debug);
             WhiteListItems = new List<Item>();
-            int num2 = 0;
-            int actualCapacity = WhiteListItems.Count + 1;
-            int rows = 3;
+            //int num2 = 0;
+            //int actualCapacity = WhiteListItems.Count + 1;
+            //int rows = 3;
             //ItemsToGrabMenu = new InventoryMenu(Game1.uiViewport.Width / 2 - num2 / 2, yPositionOnScreen + 64, playerInventory: false, WhiteListItems, InventoryMenu.highlightAllItems, actualCapacity, rows);
             ReCreateUI();
         }
-
+        
         private void ReCreateUI()
         {
-            Game1.activeClickableMenu = ItemsToGrabMenu;
-            ui = new RootElement();
-            ui.LocalPosition = new Vector2(xPositionOnScreen, yPositionOnScreen);
+            ui = new RootElement()
+            {
+                LocalPosition = new Vector2(this.xPositionOnScreen, this.yPositionOnScreen)
+            };
 
             var title = new Label()
             {
-                String = "This is a menu Title",
+                String = "Inserter Configuration",
                 Bold = true,
             };
-            title.LocalPosition = new Vector2((width - title.Width) / 2, 10);
+            title.LocalPosition = new Vector2((width - title.Width) / 2, 10 + heightOffset);
             ui.AddChild(title);
 
             //table = new Table()
@@ -80,7 +81,7 @@ namespace ExampleMod1
                 String = "Direction:",
                 Bold = true,
             };
-            DirectionText.LocalPosition = new Vector2(10, height - 250);
+            DirectionText.LocalPosition = new Vector2(15, 100 + heightOffset);
             ui.AddChild(DirectionText);
 
             int moveOverConstant = 300;
@@ -91,7 +92,7 @@ namespace ExampleMod1
                 Bold = this.inserterInstance != null ? (int)this.inserterInstance.FacingDirection == (int)Directions.SouthToNorth : false,
                 Callback = (e) => SetInserterDirection(Directions.SouthToNorth),
             };
-            SouthToNorth.LocalPosition = new Vector2((width - SouthToNorth.Width) / 2 - moveOverConstant, height - 200);
+            SouthToNorth.LocalPosition = new Vector2((width - SouthToNorth.Width) / 2 - moveOverConstant, 150 + heightOffset);
             ui.AddChild(SouthToNorth);
 
             var EastToWest = new Label()
@@ -101,7 +102,7 @@ namespace ExampleMod1
                 Bold = this.inserterInstance != null ? (int)this.inserterInstance.FacingDirection == (int)Directions.EastToWest : false,
                 Callback = (e) => SetInserterDirection(Directions.EastToWest),
             };
-            EastToWest.LocalPosition = new Vector2((width - EastToWest.Width) / 2 - 70 - moveOverConstant, height - 150);
+            EastToWest.LocalPosition = new Vector2((width - EastToWest.Width) / 2 - 70 - moveOverConstant, 200 + heightOffset);
             ui.AddChild(EastToWest);
             var WestToEast = new Label()
             {
@@ -110,7 +111,7 @@ namespace ExampleMod1
                 Bold = this.inserterInstance != null ? (int)this.inserterInstance.FacingDirection == (int)Directions.WestToEast : false,
                 Callback = (e) => SetInserterDirection(Directions.WestToEast),
             };
-            WestToEast.LocalPosition = new Vector2((width - WestToEast.Width) / 2 + 70 - moveOverConstant, height - 150);
+            WestToEast.LocalPosition = new Vector2((width - WestToEast.Width) / 2 + 70 - moveOverConstant, 200 + heightOffset);
             ui.AddChild(WestToEast);
 
             var NorthToSouth = new Label()
@@ -121,40 +122,110 @@ namespace ExampleMod1
                 Bold = this.inserterInstance != null ? (int)this.inserterInstance.FacingDirection == (int)Directions.NorthToSouth : false,
                 Callback = (e) => SetInserterDirection(Directions.NorthToSouth),
             };
-            NorthToSouth.LocalPosition = new Vector2((width - NorthToSouth.Width) / 2 - moveOverConstant, height - 100);
+            NorthToSouth.LocalPosition = new Vector2((width - NorthToSouth.Width) / 2 - moveOverConstant, 250 + heightOffset);
             ui.AddChild(NorthToSouth);
             var accept = new Label()
             {
                 String = "Accept",
                 Bold = true,
-                LocalPosition = new Vector2((width - SouthToNorth.Width)-150, height - 50),
                 Callback = (e) => Accept(),
             };
+
+            accept.LocalPosition = new Vector2((width - accept.Width) - 150, height / 2 + 5 + heightOffset);
             ui.AddChild(accept);
 
-            //if (heldItem != null)
-            //{
-            //    heldItem.drawInMenu(b, new Vector2(Game1.getOldMouseX() + 8, Game1.getOldMouseY() + 8), 1f);
-            //}
-            //ui.AddChild(ItemsToGrabMenu.)
+            var itemSlot = new ItemSlot()
+            {
+                LocalPosition = new Vector2(150, height / 2 - 50 + heightOffset),
+            };
+            ui.AddChild(itemSlot);
+
         }
 
         public InserterCustomUI(InserterObject instance): this()
         {
             inserterInstance = instance;
-            ModEntry._Monitor.Log($"InstanceSet", LogLevel.Debug);
-
             ReCreateUI();
 
         }
         public override void draw(SpriteBatch b)
         {
-            IClickableMenu.drawTextureBox(b, xPositionOnScreen - 12, yPositionOnScreen - 12, width + 24, height + 24, Color.White);
+            IClickableMenu.drawTextureBox(b, xPositionOnScreen, yPositionOnScreen + heightOffset, width, height / 2, Color.White);
+            IClickableMenu.drawTextureBox(b, xPositionOnScreen + width-310, yPositionOnScreen + height / 2 - 130, 170,75, Color.White);
+            base.draw(b, false, false);
 
             ui.Draw(b);
+            if (base.heldItem != null)
+            {
+                base.heldItem.drawInMenu(b, new Vector2(Game1.getOldMouseX() + 8, Game1.getOldMouseY() + 8), 1f);
+            }
 
             drawMouse(b);
         }
+
+        //public override void receiveLeftClick(int x, int y, bool playSound = true)
+        //{
+        //    if (!okButton.containsPoint(x, y))
+        //        base.receiveLeftClick(x, y, playSound);
+
+        //    Item held = heldItem;
+            //if (mainSpot.containsPoint(x, y))
+            //{
+            //    if (secondarySpot.item == null)
+            //    {
+            //        var tmp = mainSpot.item;
+            //        mainSpot.item = heldItem;
+            //        heldItem = tmp;
+            //    }
+            //    else
+            //    {
+            //        Game1.playSound("cancel");
+            //    }
+            //}
+            //else if (secondarySpot.containsPoint(x, y) && mainSpot.item != null)
+            //{
+            //    bool doIt = false;
+            //    if (mainSpot.item is MeleeWeapon mw)
+            //    {
+            //        if (Utility.IsNormalObjectAtParentSheetIndex(heldItem, StardewValley.Object.prismaticShardIndex))
+            //            doIt = true;
+            //        else if (heldItem is IDGAItem dgai && dgai.FullId == ItemIds.SoulSapphire && (mw.InitialParentTileIndex == 62 || mw.InitialParentTileIndex == 63 || mw.InitialParentTileIndex == 64))
+            //            doIt = true;
+            //    }
+            //    else if (mainSpot.item is Tool)
+            //    {
+            //        if (Utility.IsNormalObjectAtParentSheetIndex(heldItem, StardewValley.Object.prismaticShardIndex))
+            //            doIt = true;
+            //    }
+            //    else
+            //    {
+            //        if (heldItem is IDGAItem dgai && dgai.FullId == ItemIds.PersistiumDust)
+            //            doIt = true;
+            //    }
+
+            //    if (doIt)
+            //    {
+            //        var tmp = secondarySpot.item;
+            //        secondarySpot.item = heldItem;
+            //        heldItem = tmp;
+            //    }
+            //    else
+            //    {
+            //        Game1.playSound("cancel");
+            //    }
+            //}
+            //else if (okButton.containsPoint(x, y))
+            //{
+            //    if (mainSpot.item != null && secondarySpot.item != null && Game1.player.hasItemInInventory(ItemIds.StellarEssence.GetDeterministicHashCode(), 25))
+            //    {
+            //        doingStars = 0;
+            //    }
+            //    else
+            //    {
+            //        Game1.playSound("cancel");
+            //    }
+            //}
+        //}
 
         public override void update(GameTime time)
         {
